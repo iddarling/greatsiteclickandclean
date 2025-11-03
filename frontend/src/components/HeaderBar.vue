00d3c25bcbd7ed7cb206ff63d97f23f16f03c6a1
iddarling <stepan666bal@gmail.com>
2025-11-03 19:26:46 +0500

    добавили лого и название

--- frontend/src/components/HeaderBar.vue
+++ frontend/src/components/HeaderBar.vue
@@ -1,7 +1,9 @@
 <template>
   <header class="container header">
     <div class="brand">
-      <div style="width:40px;height:40px;border-radius:12px;background:#f2fff8;display:grid;place-items:center;">🧺</div>
+      <div style="width:40px;height:40px;border-radius:12px;background:#f2fff8;display:grid;place-items:center;">
+        <img src="/src/assets/logo.jpeg" alt="Logo" style="max-width:100%;max-height:100%;border-radius:50%;" />
+      </div>
       <span class="brand-title">Click&Clean</span>
     </div>
 
